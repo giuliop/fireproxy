1dad94150ce7b92aea558405ab502d707f03a643
giulio <giuliop@gmail.com>
2020-09-23 22:15:04 +0200

    fixed startup where proxy was shown as on but was off

--- background.js
+++ background.js
@@ -10,28 +10,34 @@ let offSettings = {
     proxyType: "none",
 };
 
-// default is on
-let currentSettings = onSettings;
-browser.browserAction.setIcon({path: "icons/on_32.png"});
-browser.browserAction.setTitle({title: "proxy on"});
-
-// toggle between on and off settings
-function toggleSettings() {
-
-    if (currentSettings.proxyType === "none") {
-
+// set proxy on
+function proxyOn() {
         currentSettings = onSettings;
         browser.proxy.settings.set({value : onSettings});
         browser.browserAction.setIcon({path: "icons/on_32.png"});
         browser.browserAction.setTitle({title: "proxy on"});
+}
 
-    } else {
+// set proxy off
+function proxyOff() {
         currentSettings = offSettings;
         browser.proxy.settings.set({value : offSettings});
         browser.browserAction.setIcon({path: "icons/off_32.png"});
         browser.browserAction.setTitle({title: "proxy off"});
+}
+
+// toggle between on and off settings
+function toggleSettings() {
+    if (currentSettings.proxyType === "none") {
+        proxyOn();
+    } else {
+        proxyOff();
     }
 }
 
 // On click, call toggleSettings
 browser.browserAction.onClicked.addListener(toggleSettings);
+
+// default is on
+proxyOn();
+
