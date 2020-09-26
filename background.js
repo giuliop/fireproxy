391b716e024e86a96db525e68c0ca4d72f824971
giulio <giuliop@gmail.com>
2020-09-26 23:10:53 +0200

    When Firefox is restarted the proxy keeps the last setting instead of defaulting to off

--- background.js
+++ background.js
@@ -12,32 +12,73 @@ let offSettings = {
 
 // set proxy on
 function proxyOn() {
-        currentSettings = onSettings;
-        browser.proxy.settings.set({value : onSettings});
-        browser.browserAction.setIcon({path: "icons/on_32.png"});
-        browser.browserAction.setTitle({title: "proxy on"});
+    browser.proxy.settings.set({value : onSettings});
+    browser.browserAction.setIcon({path: "icons/on_32.png"});
+    browser.browserAction.setTitle({title: "proxy on"});
+    let settingState = browser.storage.local.set({
+        proxyState : "on"
+    });
+    settingState.then(onSet, onSetError);
+    state = "on";
 }
 
 // set proxy off
 function proxyOff() {
-        currentSettings = offSettings;
-        browser.proxy.settings.set({value : offSettings});
-        browser.browserAction.setIcon({path: "icons/off_32.png"});
-        browser.browserAction.setTitle({title: "proxy off"});
+    browser.proxy.settings.set({value : offSettings});
+    browser.browserAction.setIcon({path: "icons/off_32.png"});
+    browser.browserAction.setTitle({title: "proxy off"});
+    let settingState = browser.storage.local.set({
+        proxyState : "off"
+    });
+    settingState.then(onSet, onSetError);
+    state = "off";
 }
 
 // toggle between on and off settings
 function toggleSettings() {
-    if (currentSettings.proxyType === "none") {
+    if (state === "off") {
         proxyOn();
     } else {
         proxyOff();
     }
 }
 
+// set desired state
+function setSettings() {
+    if (state === "off") {
+        proxyOff();
+    } else {
+        proxyOn();
+    }
+}
+
+// define callback functions for reading and writing local storage
+function onGetError(error) {
+    console.log(`Error: ${error}`);
+    setSettings();
+}
+
+function onSetError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+    //console.log(item);
+    if (item.proxyState) {
+        state = item.proxyState;
+    }
+    setSettings();
+}
+
+function onSet(item) {
+}
+
 // On click, call toggleSettings
 browser.browserAction.onClicked.addListener(toggleSettings);
 
-// default is on
-proxyOn();
+// at startup retrieve last state or start with proxy off
+state = "off";
+
+let gettingState = browser.storage.local.get("proxyState");
+gettingState.then(onGot, onGetError);
 
